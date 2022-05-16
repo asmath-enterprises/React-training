a20d7f9b38a800cad38925158eab3fcd454175e3
Asmaths <asmath3026@gmail.com>
2022-05-16 18:08:27 +0530

    firstday

--- README.md
+++ README.md
@@ -31,40 +31,12 @@ See the section about [deployment](https://facebook.github.io/create-react-app/d
 
 ### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+## Lesson about Props:
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+Within the single file `App.js` adding the components and pass the property data to the component by using `Props concept` and then i call the button in the `index.js`.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+### Notes:
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+`Mybtn` is a component to make own button.
+In `Btndynamic` I call the component `Mybtn` on that i pass the property data.
+In `Btndynamic` i added arithmetic Operations like addition,subraction,multiplication,divison and then i call the function by passing the props to `Mybtn`.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const app = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Arithmetic Operation</h1>
     </div>
   );
-}
+};
+export default app;
 
-export default App;
+const Mybtn = (props) => {
+  return (
+    <div>
+      <button onClick={props.clickMe} style={props.btnstyle}>
+        {props.text}
+      </button>
+    </div>
+  );
+};
+
+export const Btndynamic = () => {
+  const Addbtn = (a, b) => {
+    alert(a + b);
+  };
+  const Subbtn = (a, b) => {
+    alert(a - b);
+  };
+  const Mulbtn = (a, b) => {
+    alert(a * b);
+  };
+  const Divbtn = (a, b) => {
+    alert(a / b);
+  };
+  return (
+    <div>
+      <Mybtn
+        text="click me to add"
+        clickMe={() => Addbtn(10, 20)}
+        btnstyle={{
+          marginBottom: "20px",
+          fontWeight: "400",
+          color: "white",
+          lineHeight: "1.5",
+          textAlign: "center",
+          textDecoration: "none",
+          verticalAlign: "middle",
+          cursor: "pointer",
+          backgroundColor: "#0d6efd",
+          border: "1px solid transparent",
+          padding: "0.375rem 0.75rem",
+          fontSize: "1rem",
+          borderRadius: "0.25rem",
+        }}
+      />
+      <Mybtn
+        text="click me to Sub"
+        clickMe={() => Subbtn(30, 40)}
+        btnstyle={{
+          marginBottom: "20px",
+          fontWeight: "400",
+          color: "white",
+          lineHeight: "1.5",
+          textAlign: "center",
+          textDecoration: "none",
+          verticalAlign: "middle",
+          cursor: "pointer",
+          backgroundColor: "#0d6efd",
+          border: "1px solid transparent",
+          padding: "0.375rem 0.75rem",
+          fontSize: "1rem",
+          borderRadius: "0.25rem",
+        }}
+      />
+      <Mybtn
+        text="click me to Multiply"
+        clickMe={() => Mulbtn(40, 10)}
+        btnstyle={{
+          marginBottom: "20px",
+          fontWeight: "400",
+          color: "white",
+          lineHeight: "1.5",
+          textAlign: "center",
+          textDecoration: "none",
+          verticalAlign: "middle",
+          cursor: "pointer",
+          backgroundColor: "#0d6efd",
+          border: "1px solid transparent",
+          padding: "0.375rem 0.75rem",
+          fontSize: "1rem",
+          borderRadius: "0.25rem",
+        }}
+      />
+      <Mybtn
+        text="click me to divison"
+        clickMe={() => Divbtn(1, 10.5)}
+        btnstyle={{
+          marginBottom: "20px",
+          fontWeight: "400",
+          color: "white",
+          lineHeight: "1.5",
+          textAlign: "center",
+          textDecoration: "none",
+          verticalAlign: "middle",
+          cursor: "pointer",
+          backgroundColor: "#0d6efd",
+          border: "1px solid transparent",
+          padding: "0.375rem 0.75rem",
+          fontSize: "1rem",
+          borderRadius: "0.25rem",
+        }}
+      />
+    </div>
+  );
+};
